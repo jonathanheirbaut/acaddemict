2a3bfea1efa919a954535dc9306ff9773f695903
almau78 <arne.lammens@gmail.com>
2014-09-10 10:07:19 +0200

    DBUnit using sql dialect (cherry picked from commit bed1137)

--- src/test/java/com/realdolmen/jpa/DataSetPersistenceTest.java
+++ src/test/java/com/realdolmen/jpa/DataSetPersistenceTest.java
@@ -1,9 +1,11 @@
 package com.realdolmen.jpa;
 
+import org.dbunit.database.DatabaseConfig;
 import org.dbunit.database.DatabaseConnection;
 import org.dbunit.database.IDatabaseConnection;
 import org.dbunit.dataset.IDataSet;
 import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.dbunit.ext.mysql.MySqlDataTypeFactory;
 import org.dbunit.operation.DatabaseOperation;
 import org.slf4j.LoggerFactory;
 import org.junit.Before;
@@ -24,6 +26,11 @@ public class DataSetPersistenceTest extends PersistenceTest {
         Connection jdbcConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "root");
         IDatabaseConnection connection = new DatabaseConnection(jdbcConnection);
         IDataSet dataSet = new FlatXmlDataSetBuilder().build(getClass().getResource("/data.xml"));
+
+        //set factorytype in dbconfig to remove warning
+        DatabaseConfig dbConfig = connection.getConfig();
+        dbConfig.setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new MySqlDataTypeFactory());
+
         DatabaseOperation.CLEAN_INSERT.execute(connection, dataSet);
     }
 }
